37a26b44c2ebeaf5804fa3c52776d5a5d7b59345
Md Hasan Rahman <sawanbearn08@gmail.com>
2025-10-02 09:25:26 +0100

    request will always be ok

--- js/earning.js
+++ js/earning.js
@@ -2,9 +2,11 @@ document.getElementById("earningForm").addEventListener("submit", async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const month = document.getElementById("monthSelect").value;
-    const success = await sendData("earning", formData, month);
+
+    await sendData("earning", formData, month);
+
     const msg = e.target.querySelector(".message");
-    msg.textContent = success ? "Earning added ✅" : "Error ❌";
-    msg.className = success ? "message success" : "message error";
-    if (success) e.target.reset();
+    msg.textContent = "Earning added ✅";
+    msg.className = "message success";
+    e.target.reset();
 });
